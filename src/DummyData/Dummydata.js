4ee98a13af8dba26f5d7a663ff0701cacbd88319
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2024-10-23 15:17:58 +0530

    added all Files

---
+++ src/DummyData/Dummydata.js
@@ -0,0 +1,65 @@
+export const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#Services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/Kids",
+  },
+  {
+    id: 4,
+    name: "Men's Wear",
+    link: "/men",
+  },
+  {
+    id: 5,
+    name: "Electronics",
+    link: "/Electronics",
+  },
+];
+
+export const DropDownLinks = [
+  {
+    id: 1,
+    name: "Treanding Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
+
+export const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
